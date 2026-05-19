45a413802bdabed051f546b0db462a92403ab1ab
zingrin <jerinloncheu@gmail.com>
2026-05-19 11:53:08 +0600

    group declrartion

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+// var name string ="meow";
+// name:= "meow"
+
+// group declration
+var(
+	name string = "meow"
+	age int = 30
+)
+
+// multiple
+
+var x,y int
+x = 25
+y = 70
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(x,y)
+
+}
